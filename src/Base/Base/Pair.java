87194b6019a4bf12991dcd4dcbd2f56a430503a9
congpeitong <congpeitong@shundesoft.com>
2022-02-15 13:48:07 +0800

    定义了简单的泛型类

---
+++ src/Base/Base/Pair.java
@@ -0,0 +1,37 @@
+package Base.Base;
+
+/**
+ * @createUser： congpeitong
+ * @createTime： 2022/2/15
+ * @describe：
+ */
+public class Pair<T> {
+    private T first;
+    private T second;
+
+    public Pair() {
+        first = null;
+        second = null;
+    }
+
+    public Pair(T first, T second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public void setFirst(T first) {
+        this.first = first;
+    }
+
+    public T getSecond() {
+        return second;
+    }
+
+    public void setSecond(T second) {
+        this.second = second;
+    }
+}
