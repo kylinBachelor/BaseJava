b360aa6a213ab88b95aaa4466f1221f55d51862d
congpeitong <congpeitong2022@163.com>
2022-08-17 10:34:15 +0800

    java编程思想第一个程序

---
+++ src/ProgrammingIdea/FirstApplet.java
@@ -0,0 +1,13 @@
+package ProgrammingIdea;
+
+import java.util.Date;
+
+/**
+ * @author congpeitong
+ * @date 2022-08-17 10:22
+ */
+public class FirstApplet {
+    public static void main(String[] args) {
+        System.out.println("hello now time is: " + new Date().toString());
+    }
+}
