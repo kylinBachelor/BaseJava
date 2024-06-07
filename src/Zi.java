e67c80e1fd233c1326b871c1687460beb4311b12
congpeitong <congpeitong2022@163.com>
2024-06-07 08:35:43 +0800

    commit

---
+++ src/Zi.java
@@ -0,0 +1,9 @@
+public class Zi extends Fu{
+
+    String a;
+    String b;
+    String hello;
+    public Zi(){
+        System.out.println("Zi无参构造");
+    }
+}
--- src/test.java
+++ src/test.java
@@ -1,10 +1,16 @@
+import java.util.function.Consumer;
+class test {
+    public static void main(String[] args) {
+        String str = "fjsldjflsdfk>sdlfjksdfjlkf";
+        String insert = "插入的值";
+        int index = str.indexOf(">11111111");
+        System.out.println(index);
+
+        StringBuilder sb = new StringBuilder(str);
+        sb.insert(index, insert);
+        System.out.println(sb.toString());
 
 
-public class test {
-    public static void main(String[] args) throws InterruptedException {
-        String str = "1223456_0123";
-        String[] strArr = str.split("_");
-        System.out.println(strArr);
-
+        
     }
 }
